nes-linux/kdump-utils

commit 3f4a9eb4dbf913425d0139d09ca33093ff2b9e7a

Bring up the network before Open vSwitch bridge
Resolves: https://issues.redhat.com/browse/RHEL-75547

Currently, kdump tries to bring up the same network as in 1st kernel as
we assume if the same network works in the 1st kernel, there is no
reason it won't work in 1st kernel.

But for Open vSwitch (OVS) bridge, there are two challenges,
1. Extra memory requirement
   For example, crashkernel=260M will be needed for OVS bridge while
   only crashkernel=192M is sufficient for default network for F40 cloud
   image.

2. A bit complex dependencies
   An extra dracut module is needed to install dependent executables,
   helper scripts, configuration files and a NetworkManager plugin.

To avoid the above challenges, we try to bring up the network before OVS
bridge instead. OVS bridge is set up by ovs-configuration.service
[1][2], we can assume the previous network has worked.

When ovs-configuration.service is started, it will bring down the
original connection profiles. So we should go through the inactive
connection profiles to find out what connection profiles is for bringing
up the physical network interface.

How to test this patch with the help of configure-ovs.sh?
=========================================================

1. Extract configure-ovs.sh from [2]

2. Install necessary packages for configure-ovs.sh
    dnf install openvswitch -yq
    dnf install NetworkManager-ovs nmap-ncat -yq

    systemctl enable --now openvswitch

    # restart NM so the ovs plugin can be activated
    systemctl restart NetworkManager

3. Assume the network interface used for creating an Ovs bridge is
   "ens2", use configure-ovs.sh to create an Ovs bridge,

    interface=ens2
    mkdir -p /etc/ovnk
    echo $interface > /etc/ovnk/iface_default_hint
    bash configure-ovs.sh OVNKubernetes

4. (Optional) If you want to make the created Ovs bridge survive a
   reboot, simply make the created NM connections created by
   configure-ovs.sh persist,

dracut/99kdumpbase/module-setup.sh
@@ -3,11 +3,15 @@
 _DRACUT_KDUMP_NM_TMP_DIR="$DRACUT_TMPDIR/$$-DRACUT_KDUMP_NM"
 
 _save_kdump_netifs() {
-    unique_netifs[$1]=1
+    if [[ $is_ovs_bridge == 1 ]]; then
+        ovs_unique_netifs[$1]=1
+    else
+        unique_netifs[$1]=1
+    fi
 }
 
 _get_kdump_netifs() {
-    echo -n "${!unique_netifs[@]}"
+    echo -n "${!unique_netifs[@]} ${!ovs_unique_netifs[@]}"
 }
 
 kdump_module_init() {
@@ -320,8 +324,29 @@ _install_nmconnection() {
     inst "$_src" "$_dst"
 }
 
+# Install the original connection profile that was active before
+# ovs-configuration.service
+_install_nmconnections_before_ovs() {
+    local _nm_conn_path _cloned_nm_path _nic_name
+
+    ((${#ovs_unique_netifs[@]})) || return
+
+    while IFS=: read -r _nm_conn_path _; do
+        _nic_name=$(nmcli --get-values connection.interface-name connection show "$_nm_conn_path")
+        [[ -v "ovs_unique_netifs[$_nic_name]" ]] || continue
+        if _cloned_nm_path=$(clone_and_modify_nmconnection "$_nic_name" "$_nm_conn_path"); then
+            _install_nmconnection "$_cloned_nm_path"
+        else
+            derror "Failed to install the .nmconnection for $_nic_name"
+            exit 1
+        fi
+    done <<<"$(nmcli --get-values filename,ACTIVE connection show | grep "no$")"
+}
+
 kdump_install_nmconnections() {
     local _netif _nm_conn_path _cloned_nm_path
+
+    _install_nmconnections_before_ovs
 
     while IFS=: read -r _netif _nm_conn_path; do
         [[ -v "unique_netifs[$_netif]" ]] || continue
@@ -524,10 +549,32 @@ kdump_get_remote_ip() {
     echo "$_remote"
 }
 
+# Find the physical interface of Open vSwitch (Ovs) bridge
+#
+# The physical network interface has the same MAC address as the Ovs bridge
+ovs_find_phy_if() {
+    local _mac _dev
+    _mac=$(kdump_get_mac_addr "$1")
+
+    for _dev in $(ovs-vsctl list-ifaces "$1"); do
+        if [[ $_mac == $(< /sys/class/net/"$_dev"/address) ]]; then
+            echo -n "$_dev"
+            return
+        fi
+    done
+
+    return 1
+}
+
+# Tell if a network interface is an Open vSwitch (Ovs) bridge
+kdump_is_ovs_bridge() {
+    [[ $(_get_nic_driver "$1") == openvswitch ]]
+}
+
 # Collect netifs needed by kdump
 # $1: destination host
 kdump_collect_netif_usage() {
-    local _destaddr _srcaddr _route _netdev
+    local _destaddr _srcaddr _route _netdev is_ovs_bridge
 
     _destaddr=$(kdump_get_remote_ip "$1")
 
@@ -538,6 +585,11 @@ kdump_collect_netif_usage() {
 
     _srcaddr=$(kdump_get_ip_route_field "$_route" "src")
     _netdev=$(kdump_get_ip_route_field "$_route" "dev")
+
+    if kdump_is_ovs_bridge "$_netdev"; then
+        is_ovs_bridge=1
+        _netdev=$(ovs_find_phy_if "$_netdev")
+    fi
 
     if kdump_is_bridge "$_netdev"; then
         kdump_setup_bridge "$_netdev"
@@ -1018,7 +1070,7 @@ EOF
 }
 
 install() {
-    declare -A unique_netifs ipv4_usage ipv6_usage
+    declare -A unique_netifs ovs_unique_netifs ipv4_usage ipv6_usage
 
     kdump_module_init
     kdump_install_conf

commit 3702c95dfc896e95ea9cdbaa57613c360362d5e7

99kdumpbase: Fix attempting to setup kdump network if no network interface is used
kdump_install_net() also attempts to setup kdump network if no network
interface is used.

This is because _get_kdump_netifs() always outputs not empty string.
When both unique_netifs and ovs_unique_netifs are empty,
_get_kdump_netifs() incorrectly outputs " " (one space).
In that case, it is expected to output empty string.

This may cause that unnecessary network-related files are installed to
initramfs.

This patch fixes this issue by correcting _get_kdump_netifs() output.

Fixes: 3f4a9eb4dbf9 ("Bring up the network before Open vSwitch bridge")
Signed-off-by: Kazuma Kondo <kazuma-kondo@nec.com>

dracut/99kdumpbase/module-setup.sh
@@ -11,7 +11,8 @@ _save_kdump_netifs() {
 }
 
 _get_kdump_netifs() {
-    echo -n "${!unique_netifs[@]} ${!ovs_unique_netifs[@]}"
+    local _netifs=(${!unique_netifs[@]} ${!ovs_unique_netifs[@]})
+    echo -n ${_netifs[@]}
 }
 
 kdump_module_init() {